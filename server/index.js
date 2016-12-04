4a79b929595ff1fea40c89881c15e14838b10f4f
Matt <mawburn7@gmail.com>
2016-12-03 23:47:42 -0600

    More grossness, needs refactor

--- server/index.js
+++ server/index.js
@@ -7,9 +7,16 @@ require('dotenv').config()
 var googleKey = process.env.GOOGLE_KEY
 var winston = require('winston')
 require('winston-daily-rotate-file')
+var RateLimit = require('express-rate-limit');
+var fs = require('fs')
+var logDir = './logs'
+
+if(!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir)
+}
 
 var transport = new winston.transports.DailyRotateFile({
-    filename: './log',
+    filename: `${logDir}/log`,
     datePattern: 'yyyy-MM-dd.',
     prepend: true,
     level: 'info'
@@ -21,18 +28,23 @@ var logger = new (winston.Logger)({
     ]
   })
 
-
+var limiter = new RateLimit({
+  windowMs: 10*60*1000,  
+  max: 100, 
+  delayMs: 500 
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(limiter)
 
 app.post('/', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   var lat = req.body.lat
   var long = req.body.long
   var type = req.body.type
 
-  console.log(lat, long, type)
-  logger.info({message: {lat, long, type}})
+  logger.info({message: {ip, lat, long, type}})
 
   fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=8500&type=restaurant&keyword=${type}&key=${googleKey}`)
     .then(gRes => {

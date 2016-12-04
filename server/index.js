b55ec120c2b7e8efa0a1d9e7e6b98853214139b8
Matt <mawburn7@gmail.com>
2016-12-04 01:14:51 -0600

    Minor refactoring

--- server/index.js
+++ server/index.js
@@ -1,37 +1,36 @@
-var server = require('express')
-var app = server()
-var port = process.env.PORT || 4242
-var bodyParser = require('body-parser')
-var fetch = require('isomorphic-fetch')
+const fs = require('fs')
+const server = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('isomorphic-fetch')
+const RateLimit = require('express-rate-limit');
+const winston = require('winston')
 require('dotenv').config()
-var googleKey = process.env.GOOGLE_KEY
-var winston = require('winston')
 require('winston-daily-rotate-file')
-var RateLimit = require('express-rate-limit');
-var fs = require('fs')
-var logDir = './logs'
+
+const app = server()
+const port = process.env.PORT || 4242
+const googleKey = process.env.GOOGLE_KEY
+const logDir = './logs'
 
 if(!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir)
 }
 
-var transport = new winston.transports.DailyRotateFile({
-    filename: `${logDir}/log`,
-    datePattern: 'yyyy-MM-dd.',
-    prepend: true,
-    level: 'info'
-  })
+const transport = new winston.transports.DailyRotateFile({
+  filename: `${logDir}/log`,
+  datePattern: 'yyyy-MM-dd.',
+  prepend: true,
+  level: 'info'
+})
 
-var logger = new (winston.Logger)({
-    transports: [
-      transport
-    ]
-  })
+const logger = new (winston.Logger)({
+  transports: [transport]
+})
 
-var limiter = new RateLimit({
+const limiter = new RateLimit({
   windowMs: 10*60*1000,  
-  max: 100, 
-  delayMs: 500 
+  max: 20, 
+  delayMs: 1000 
 })
 
 app.use(bodyParser.json())
@@ -39,14 +38,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(limiter)
 
 app.post('/', (req, res) => {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-  var lat = req.body.lat
-  var long = req.body.long
-  var type = req.body.type
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  let lat = req.body.lat
+  let long = req.body.long
 
-  logger.info({message: {ip, lat, long, type}})
+  logger.info({message: {ip, lat, long}})
 
-  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=8500&type=restaurant&keyword=${type}&key=${googleKey}`)
+  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&rankby=prominence&opennow=true&radius=8500&type=restaurant&key=${googleKey}`)
     .then(gRes => {
       return gRes.json()
     })

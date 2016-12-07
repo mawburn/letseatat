ada66b792c370cc30e656b0a5ca8c6f6b96260be
Matt <mawburn7@gmail.com>
2016-12-06 21:34:02 -0600

    Async/Await - Needs refactor, but works

--- server/index.js
+++ server/index.js
@@ -40,27 +40,51 @@ app.use(limiter)
 app.use(cors())
 
 app.post('/', (req, res) => {
-  console.log(req.body)
   let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   let lat = req.body.lat
   let long = req.body.long
 
-  logger.info({message: {ip, lat, long}})
-
-  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&rankby=prominence&opennow=true&radius=8500&type=restaurant&key=${googleKey}`)
-    .then(gRes => gRes.json())
-    .then(json => {
-      let newData = json.results.map(place => {
-        let weight = Math.round(place.rating) || 2
-        return {name: place.name, weight}
-      })
-
-      Promise.all(newData).then(() => {
-        res.send(newData)
-      })
+  searchPlaces(`location=${lat},${long}&rankby=prominence&opennow=true&radius=8500&type=restaurant&key=${googleKey}`)
+    .then(resp => {
+      res.send(resp)
     })
 })
 
 app.listen(port, () => {
   console.log()
 })
+
+async function searchPlaces(params) {
+  const baseUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`
+  const page1 = await fetch(`${baseUrl}${params}`).then(res => res.json())
+  await sleep(1500) // fucking Google API timeout
+  const page2 = await fetch(`${baseUrl}pagetoken=${page1['next_page_token']}&key=${googleKey}`).then(res => res.json())
+  await sleep(1500) // uggg
+  const page3 = await fetch(`${baseUrl}pagetoken=${page2['next_page_token']}&key=${googleKey}`).then(res => res.json())
+  
+  const output1 = await page1.results.map((place) => {
+    let weight = Math.round(place.rating)
+    return { name: place.name, weight}
+  })
+
+  const output2 = await page2.results.map((place) => {
+    let weight = Math.round(place.rating)
+    return { name: place.name, weight}
+  })
+
+  const output3 = await page3.results.map((place) => {
+    let weight = Math.round(place.rating)
+    return { name: place.name, weight}
+  })
+
+  let output = output1.concat(output2).concat(output3)
+
+  return output.filter((obj, pos, arr) => {
+    return arr.map(mapObj => mapObj.name).indexOf(obj.name) === pos;
+  })
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
